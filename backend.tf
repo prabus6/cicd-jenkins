e24e607567dd655bc75fb4499e1bda0ff934a757
Prabu <sprabu825@gmail.com>
2023-07-30 13:25:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket"
+    bucket = "ps6-terraform-state-bucket"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table"
